a499973c4b84054e1173aa250e6e3df68148e97c
Jeisy Mora <jeisysherlin122@gmail.com>
2025-06-23 01:23:01 -0600

    fx_se elimina decorador goblal

--- src/modules/prisma.module.ts
+++ src/modules/prisma.module.ts
@@ -1,6 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from 'src/infrastructure/services/prisma.service';
-@Global()
+
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
