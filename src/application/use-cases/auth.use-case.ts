368e293f5fbfffd4534d0b627b98b60f56ead43a
sar122 <lisa797.est@gmail.com>
2025-06-24 12:30:10 -0600

    JWT correcto

--- src/application/use-cases/auth.use-case.ts
+++ src/application/use-cases/auth.use-case.ts
@@ -1,10 +1,13 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from '../../infrastructure/database/user.repository';
 import * as bcrypt from 'bcrypt';
+import { JwtTokenService } from 'src/infrastructure/services/jwt-token.service';
 
 @Injectable()
 export class AuthUseCase {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository,
+    private readonly tokenService: JwtTokenService
+  ) {}
 
   async execute(email: string, password: string) {
     const user = await this.userRepository.findByEmail(email);
@@ -19,11 +22,19 @@ export class AuthUseCase {
       throw new Error('Contraseña incorrecta');
     }
 
+    const token = this.tokenService.generate({ 
+       sub:user.id, 
+       email: user.email
+
+    })
+
     const { password: _, ...userWithoutPassword } = user;
-    return userWithoutPassword;
-  }
-
-
+    return {
+          access_token: token
+    };
     
+  }
+  
+ 
   
 }
--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -4,10 +4,29 @@ import { AuthUseCase } from 'src/application/use-cases/auth.use-case';
 import { UserRepository } from 'src/infrastructure/database/user.repository';
 import { PrismaModule } from './prisma.module';
 import { RegisterUseCase } from 'src/application/use-cases/register.use-case';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtTokenService } from 'src/infrastructure/services/jwt-token.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), 
+    PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'), 
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthUseCase, UserRepository, RegisterUseCase],
+  providers: [
+    AuthUseCase,
+    UserRepository,
+    RegisterUseCase,
+    JwtTokenService,
+  ],
 })
 export class AuthModule {}
