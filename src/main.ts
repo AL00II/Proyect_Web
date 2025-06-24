38be959b5667854f30f54801c85884240f3a49b1
AloCJ <alondracortes0830@gmail.com>
2025-06-24 14:16:15 -0600

    Guard JWT global, itegración de Rutas Public

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { JwtAuthGuard } from './infrastructure/guards/jwt-auth.guard';
+import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const reflector = app.get(Reflector);
+
+  // Aplica el guard de JWT a toda la aplicación
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+  await app.listen(3000);
 }
 bootstrap();
